6391a7a416627154be2eef26ba0883b9a3b96c6d
Bill Weiss <bv@billweiss.net>
2012-01-18 12:03:32 -0600

    Dan suggests a couple of improvements

--- hw1/3.hs
+++ hw1/3.hs
@@ -1,5 +1,6 @@
--- I found putStr, but it doesn't print until after I enter something.
--- So, dunno how to do this without the extra line.
-main = do putStrLn "Enter your name: "
+import System.IO
+
+main = do hSetBuffering stdout NoBuffering
+          putStr "Enter your name: "
           name <- getLine
-          putStrLn ("Hello, " ++ name ++ "!")
+          putStrLn $ "Hello, " ++ name ++ "!"
