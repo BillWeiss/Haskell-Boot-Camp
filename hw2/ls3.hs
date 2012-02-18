a8264053575e2586484dbf2a34e3aeee01f53ca4
Bill Weiss <bweiss@backstopsolutions.com>
2012-02-17 20:37:13 -0600

    Broke showDirContents into two functions on Dan's suggestion

--- hw2/ls3.hs
+++ hw2/ls3.hs
@@ -3,16 +3,23 @@ module Main where
 import System.Environment
 import System.Directory
 
-showDirContents x = do
-    de <- doesDirectoryExist x
+-- Argument order is a bit screwy because I'm not sure how to make mapM
+-- insert args not at the end.  Dan will help here.
+onlyIfDirExists fn dir = do
+    de <- doesDirectoryExist dir
     if ( not de )
-        then ( putStrLn $ "Error: " ++ x ++ " does not exist!\n" )
-        else do
-            putStrLn $ "Contents of " ++ x ++ ":"
-            f <- getDirectoryContents x
+        then ( putStrLn $ "Error: " ++ dir ++ " does not exist!\n" )
+        else do fn dir
+
+showDirContents dir = do
+            putStrLn $ "Contents of " ++ dir ++ ":"
+            f <- getDirectoryContents dir
             mapM putStrLn f
             putStrLn ""
 
 main = do
     args <- getArgs
-    mapM showDirContents args
+    -- yep, right here.  I'd prefer the call to be 
+    -- onlyIfDirExists (directory) (function)
+    -- how's that work?
+    mapM ( onlyIfDirExists showDirContents ) args
