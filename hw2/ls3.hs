7499decc830e7557820542d92f508853934f5182
Bill Weiss <bweiss@backstopsolutions.com>
2012-02-17 22:57:23 -0600

    Removed comments, since I got an answer

--- hw2/ls3.hs
+++ hw2/ls3.hs
@@ -3,8 +3,6 @@ module Main where
 import System.Environment
 import System.Directory
 
--- Argument order is a bit screwy because I'm not sure how to make mapM
--- insert args not at the end.  Dan will help here.
 onlyIfDirExists fn dir = do
     de <- doesDirectoryExist dir
     if ( not de )
@@ -19,7 +17,4 @@ showDirContents dir = do
 
 main = do
     args <- getArgs
-    -- yep, right here.  I'd prefer the call to be 
-    -- onlyIfDirExists (directory) (function)
-    -- how's that work?
     mapM_ ( onlyIfDirExists showDirContents ) args
