e543a5e7325c2cacc2c611bb3e8b35e6eacc0e51
Bill Weiss <bv@billweiss.net>
2012-03-16 22:19:46 -0500

    Not awesome.  Shortened the program, but it just blows up the CPU on the machine I'm when I hit "y".  Dunno.

--- hw3/hw1.hs
+++ hw3/hw1.hs
@@ -1,16 +1,13 @@
 module Main where
 
 import System.IO
+import Control.Monad
 import Control.Applicative
-
-gobsFunction "y" = do
-    putStr "Penus "
-    gobsFunction "y"
-gobsFunction x = putStrLn "I don't know what to do with " ++ x
 
 main = do
     hSetBuffering stdout NoBuffering
     putStrLn "Gob's Program: Y/N?"
     putStr "? "
-    input <- read <$> getLine
-    gobsFunction input
+    input <- getChar
+    when (input == 'y') $ forever putStr "Penus "
+
