1e5679f2e3d07f99e91a26f1aba94cfac563492e
Bill Weiss <bv@billweiss.net>
2012-03-17 13:07:43 -0500

    WTF, Haskell.  This version works, the previous one doesn't.  Took out Control.Applicative, and added a $ to my forever line.  Don't get it.

--- hw3/hw1.hs
+++ hw3/hw1.hs
@@ -2,11 +2,9 @@ module Main where
 
 import System.IO
 import Control.Monad
-import Control.Applicative
 import Data.Char
 
--- This just runs away with the CPU right now, not awesome
-gobsFunction 'y' = forever putStr "Penus "
+gobsFunction 'y' = forever $ putStr "Penus "
 --- There wasn't really a spec for what to do if the input wasn't "y"
 gobsFunction _ = putStrLn "Dunno"
 
