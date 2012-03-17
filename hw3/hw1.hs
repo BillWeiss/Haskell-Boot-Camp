605a56ea428e0c0f90f47d9477d11b8211610b82
Bill Weiss <bv@billweiss.net>
2012-03-16 22:35:25 -0500

    Not better, but at least it has a function :)

--- hw3/hw1.hs
+++ hw3/hw1.hs
@@ -3,11 +3,16 @@ module Main where
 import System.IO
 import Control.Monad
 import Control.Applicative
+import Data.Char
+
+-- This just runs away with the CPU right now, not awesome
+gobsFunction 'y' = forever putStr "Penus "
+--- There wasn't really a spec for what to do if the input wasn't "y"
+gobsFunction _ = putStrLn "Dunno"
 
 main = do
     hSetBuffering stdout NoBuffering
     putStrLn "Gob's Program: Y/N?"
     putStr "? "
     input <- getChar
-    when (input == 'y') $ forever putStr "Penus "
-
+    gobsFunction $ toLower input
